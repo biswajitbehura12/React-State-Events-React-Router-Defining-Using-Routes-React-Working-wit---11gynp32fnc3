0d7cdd999f5f9c2ed1421042960ffd3a630c8ecb
BISWAJIT BEHURA <102812316+biswajitbehura12@users.noreply.github.com>
2022-07-01 17:11:10 +0530

    Update AppRoutes.js

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 export const AppRoutes = () => {
     return (
-<div> jnkwejssippppppppppppppppppppppppppppppppppppppppppppppppppppps</div>
+        
+        <Router>
+        <Routes>
+        <Route exact path='/' element={
+          <Index />
+        } />
+        </Routes>
+        </Router>
     )
 }
